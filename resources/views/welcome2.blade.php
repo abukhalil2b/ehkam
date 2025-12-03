470c07544cf0b80ce0eeb74e6e0eb2f4378de5f3
abukhalil2b <abukhalil2b>
2025-12-03 14:13:20 +0400

    f

--- resources/views/welcome2.blade.php
+++ resources/views/welcome2.blade.php
@@ -18,6 +18,11 @@
             href="{{ route('workshow_attendance_register') }}">تسجيل الحضور في الورشة</a>
 
     </div>
+    <div class="mt-5 max-w-7xl mx-auto bg-white rounded-xl card overflow-hidden">
+        <a class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
+            href="{{ route('login') }}">حصر اسهامات المديريات</a>
+
+    </div>
 </body>
 
 </html>
